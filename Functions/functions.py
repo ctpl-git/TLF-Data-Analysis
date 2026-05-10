9d3197a93acbc49daad901e26b1738227c1cecdc
Arjun Gautam <arjun.gautam.1694@gmail.com>
2026-05-10 13:22:52 +0545

    removed files

--- Functions/functions.py
+++
@@ -1,4 +0,0 @@
-# TODO:
-# Develop workflow for different data types (For example: read excel data, clean excel data, extract fields and data, represent fields and data into JSON file)
-# Develop a list of functions needed for the workflow
-# Code the functions and then interlink the functions as per the workflow
